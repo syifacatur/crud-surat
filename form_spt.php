c277fd8dac5fb3ee790bab6c499426d43f3071c9
syifacatur <syifatr03@gmail.com>
2024-09-23 09:20:43 +0700

    form_spt.php

---
+++ form_spt.php
@@ -0,0 +1,268 @@
+<?php 
+include 'template/header_admin.php'; 
+include 'koneksidb.php';
+?>
+
+<div class="content-wrapper">
+<section class="content-header">
+  <h1>Form_SPT 
+  </h1>
+  <ol class="breadcrumb">
+  <li><a href="index.php"><i class="fa fa-dashboard"></i> Home</a></li>
+  <li class="active">Form_SPT</li>
+  </ol>
+</section>
+<!-- Main content -->
+<section class="content">
+  <div class="row">
+    <div class="col-sm-12">
+      <div class="box box-success">
+        <div class="box-header with-border">
+          <h3 class="box-title"></h3> 
+          <div class="box-tools pull-left">
+            <a href="#" class="btn btn-success" data-toggle="modal" data-target="#tambahproduk"><i class="fa fa-plus"></i> Tambah</a>
+          </div>
+        </div>
+        <div class="box-body">
+
+          <div class="table-responsive22">
+            <table id="datatable" class="table table-bordered table-striped">
+              <thead>
+                <tr>
+                  <th>No SPT</th>
+                  <th>Dasar Undangan</th>
+                  <th>Lokasi</th>
+                  <th>Berangkat</th>
+                  <th>Pulang</th>
+                  <th>Tanggal SPT</th>
+                  <th>Anggaran</th>
+                  <th>NIP Penandatangan</th>
+                  <th>Opsi</th>
+                </tr>
+              </thead>
+              <tbody>
+                  <?php
+                  $no = 1;
+                    $queryview = mysqli_query($koneksi, "SELECT * FROM form_spt");
+                    while ($row = mysqli_fetch_assoc($queryview)) {
+                      
+                  ?>
+                  <tr>
+                    <td><?php echo $no++; ?></td>
+                    <td><?php echo $row['dasar_undangan'];?></td>
+                    <td><?php echo $row['lokasi'];?></td>
+                    <td><?php echo $row['tgl_kegiatan'];?></td>
+                    <td><?php echo $row['tgl_pulang'];?></td>
+                    <td><?php echo $row['tgl_spt'];?></td>
+                    <td><?php echo $row['anggaran'];?></td>
+                    <td><?php echo $row['NIP_penandatangan'];?></td>
+                    <td>
+                      <!--<a href="form_edituser.php?id=<?php echo $row['id_user']?>" class="btn btn-success btn-flat btn-xs"><i class="fa fa-pencil"></i> Edit</a>-->
+                      <a href="#" class="btn btn-success btn-flat btn-xs" data-toggle="modal" data-target="#updateproduk<?php echo $no; ?>"><i class="fa fa-pencil"></i> Edit</a>
+                      <a href="#" class="btn btn-danger btn-flat btn-xs" data-toggle="modal" data-target="#deleteproduk<?php echo $no; ?>"><i class="fa fa-trash"></i> Hapus</a>                       
+                      <a href=""><button class"btn btn-default btn-xs" ><i class="fa fa-print"></i> Cetak</button></a>
+                     
+
+                      
+                      <!-- modal delete -->
+                      <div class="example-modal">
+                        <div id="deleteproduk<?php echo $no; ?>" class="modal fade" role="dialog" style="display:none;">
+                          <div class="modal-dialog">
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                <h3 class="modal-title">Konfirmasi Hapus Data </h3>
+                              </div>
+                              <div class="modal-body">
+                                <h4 align="center" >Apakah anda yakin ingin menghapus <?php echo $row['no_spt'];?><strong><span class="grt"></span></strong> ?</h4>
+                              </div>
+                              <div class="modal-footer">
+                                <button id="nodelete" type="button" class="btn btn-danger pull-left" data-dismiss="modal">Cancel</button>
+                                <a href="function_spt.php?act=deleteproduk&id_spt=<?php echo $row['id_spt']; ?>" class="btn btn-success">Hapus</a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div><!-- modal delete -->
+
+                      <!-- modal update user -->
+                      <div class="example-modal">
+                        <div id="updateproduk<?php echo $no; ?>" class="modal fade" role="dialog" style="display:none;">
+                          <div class="modal-dialog">
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                <h3 class="modal-title">Edit</h3>
+                              </div>
+                              <div class="modal-body">
+                                <form action="function_spt.php?act=updateproduk" method="post" role="form">
+                                  <?php
+                                  $id_spt = $row['id_spt'];
+                                  $query = "SELECT * FROM form_spt WHERE id_spt='$id_spt'";
+                                  $result = mysqli_query($koneksi, $query);
+                                  while ($row = mysqli_fetch_assoc($result)) {
+                                  ?>
+                                  <div class="form-group">
+                                    <div class="row">
+                                      <div class="col-sm-8"><input type="hidden" class="form-control" name="id_spt" placeholder="ID spt" value="<?php echo $row['id_spt']; ?>"></div>
+                                    </div>
+                                  </div>
+                                  <div class="form-group">
+                                    <div class="row">
+                                    <label class="col-sm-3 control-label text-right">Dasar Undangan
+                                      <span class="text-red">*</span></label>
+                                    <div class="col-sm-8"><input type="text" class="form-control" name="dasar_undangan" placeholder="dasar_undangan" value="<?php echo $row['dasar_undangan']; ?>"></div>
+                                    </div>
+                                  </div>
+                                  <div class="form-group">
+                                    <div class="row">
+                                    <label class="col-sm-3 control-label text-right">Lokasi
+                                      <span class="text-red">*</span></label>
+                                    <div class="col-sm-8"><input type="text" class="form-control" name="lokasi" placeholder="lokasi" value="<?php echo $row['lokasi']; ?>"></div>
+                                    </div>
+                                  </div>
+                                  <div class="form-group">
+                                    <div class="row">
+                                    <label class="col-sm-3 control-label text-right">Berangkat
+                                      <span class="text-red">*</span></label>
+                                    <div class="col-sm-8"><input type="text" class="form-control" name="tgl_kegiatan" placeholder="tgl_kegiatan" value="<?php echo $row['tgl_kegiatan']; ?>"></div>
+                                    </div>
+                                  </div>
+                                  <div class="form-group">
+                                    <div class="row">
+                                    <label class="col-sm-3 control-label text-right">Pulang
+                                      <span class="text-red">*</span></label>
+                                    <div class="col-sm-8"><input type="text" class="form-control" name="tgl_pulang" placeholder="tgl_pulang" value="<?php echo $row['tgl_pulang']; ?>"></div>
+                                    </div>
+                                  </div>
+                                  <div class="form-group">
+                                    <div class="row">
+                                    <label class="col-sm-3 control-label text-right">Tanggal SPT
+                                      <span class="text-red">*</span></label>
+                                    <div class="col-sm-8"><input type="text" class="form-control" name="tgl_spt" placeholder="tgl_spt" value="<?php echo $row['tgl_spt']; ?>"></div>
+                                    </div>
+                                  </div>
+                                  <div class="form-group">
+                                    <div class="row">
+                                    <label class="col-sm-3 control-label text-right">Anggaran
+                                      <span class="text-red">*</span></label>
+                                    <div class="col-sm-8"><select type="text" class="form-control" name="anggaran" placeholder="anggaran" value="<?php echo $row['anggaran']; ?>"></div>
+                                    <option value="">-pilih anggaran-</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+
+                                     </select>
+                                    </div>
+                                  </div>
+                                  <div class="form-group">
+                                    <div class="row">
+                                    <label class="col-sm-3 control-label text-right">NIP Penandatangan
+                                      <span class="text-red">*</span></label>
+                                    <div class="col-sm-8"><input type="text" class="form-control" name="NIP_penandatangan" placeholder="NIP_penandatangan" value="<?php echo $row['NIP_penandatangan']; ?>"></div>
+                                    </div>
+                                  </div>
+                                  <div class="modal-footer">
+                                    <button id="noedit" type="button" class="btn btn-danger pull-left" data-dismiss="modal">Batal</button>
+                                    <input type="submit" name="submit" class="btn btn-success" value="Update">
+                                  </div>
+                                  <?php
+                                  }
+                                  ?>
+                                </form>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div><!-- modal update user -->
+                    </td>
+                  </tr>
+                  <?php
+                    }
+                  ?>
+              </tbody>
+            </table>
+          </div> 
+        </div>
+
+        <!-- modal insert -->
+        <div class="example-modal">
+          <div id="tambahproduk" class="modal fade" role="dialog" style="display:none;">
+            <div class="modal-dialog"> 
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                  <h3 class="modal-title">Tambah </h3>
+                </div>
+                <div class="modal-body">
+                  <form action="function_spt.php?act=tambahproduk" method="post" role="form">
+                    <div class="form-group">
+                      <div class="row">
+                      <label class="col-sm-3 control-label text-right">Dasar Undangan<span class="text-red">*</span></label>
+                      <div class="col-sm-8"><input type="text" class="form-control" name="dasar_undangan" placeholder="" value=""></div>
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <div class="row">
+                      <label class="col-sm-3 control-label text-right">Lokasi<span class="text-red">*</span></label>
+                      <div class="col-sm-8"><input type="text" class="form-control" name="lokasi" placeholder="" value=""></div>
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <div class="row">
+                      <label class="col-sm-3 control-label text-right">Berangkat<span class="text-red">*</span></label>
+                      <div class="col-sm-8"><input type="text" class="form-control" name="tgl_kegiatan" placeholder="" value=""></div>
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <div class="row">
+                      <label class="col-sm-3 control-label text-right">Pulang<span class="text-red">*</span></label>
+                      <div class="col-sm-8"><input type="text" class="form-control" name="tgl_pulang" placeholder="" value=""></div>
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <div class="row">
+                      <label class="col-sm-3 control-label text-right">Tanggal SPT<span class="text-red">*</span></label>
+                      <div class="col-sm-8"><input type="text" class="form-control" name="tgl_spt" placeholder="" value=""></div>
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <div class="row">
+                      <label class="col-sm-3 control-label text-right">Anggaran<span class="text-red">*</span></label>
+                      <div class="col-sm-8"><select type="text" class="form-control" name="anggaran" placeholder="" value=""></div>
+                      <option value="">-pilih anggaran-</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                  </select>
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <div class="row">
+                      <label class="col-sm-3 control-label text-right">NIP penandatangan<span class="text-red">*</span></label>
+                      <div class="col-sm-8"><input type="text" class="form-control" name="NIP_penandatangan" placeholder="" value=""></div>
+                      </div>
+                    </div>
+                    <div class="modal-footer">
+                      <button id="nosave" type="button" class="btn btn-danger pull-left" data-dismiss="modal">Batal</button>
+                      <input type="submit" name="submit" class="btn btn-success" value="Tambah">
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div><!-- modal insert close -->
+      </div>
+    </div>
+  </div>
+</section><!-- /.content -->
+</div>
+
+<?php include 'template/footer.php'; ?>
