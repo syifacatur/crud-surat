48455b6d4a8335a56d013a75a30693e7a3147655
ilham Akbar <ilhamhakbar@gmail.com>
2024-09-27 11:07:01 +0700

    surat dinamis

--- cetak_dinamis.php
+++ cetak_dinamis.php
@@ -17,22 +17,22 @@ if ($conn->connect_error) {
 }
 
 // Query untuk mengambil data dari tabel surat
-$no = 1;
-$query = "SELECT kode_produk FROM tb_produk WHERE id_produk";
+
+$query = "SELECT * FROM tb_produk ";
 $result = $conn->query($query);
 
 // Periksa apakah data ditemukan
 if ($result->num_rows > 0) {
     // Mengambil data dari query
-    $row = $result->fetch_assoc();
+    // $row = $result->fetch_assoc();
 
 
     // Membuat objek PDF
-    $pdf = new TCPDF('P','mm','Legal');
+    $pdf = new TCPDF('P', 'mm', 'Legal');
 
     //remove default header and footer
-$pdf->setPrintHeader(false);
-$pdf->setPrintFooter(false);
+    $pdf->setPrintHeader(false);
+    $pdf->setPrintFooter(false);
 
 
     // Menambahkan halaman
@@ -40,37 +40,42 @@ $pdf->setPrintFooter(false);
 
 
     //logo
-$pdf->Image('library/logo1.jpg',10,10,17);
-//title
-$pdf->SetFont('Helvetica','B',14);
-$pdf->Cell(190,5,"PEMERINTAH PROVINSI JAWA TENGAH",0,1,'C');
-$pdf->SetFont('Helvetica','B',14);
-$pdf->Cell(190,5,"BADAN PENGEMBANGAN ",0,1,'C');
-$pdf->Cell(190,5,"SUMBER DAYA MANUSIA DAERAH",0,1,'C');
-$pdf->SetFont('Helvetica','',10);
-$pdf->Cell(190,3,"Jalan Setiabudi Nomor 201 A Semarang Kode Pos 50263",0,1,'C');
-$pdf->Cell(190,3,"Telp. 024-7473066 Faks. 024-7473701",0,1,'C');
-$pdf->Cell(190,3,"Website : www.bpsdmd.jatengprov.go.id Email : bpsdmd@jatengprov.go.id",0,1,'C');
+    $pdf->Image('library/logo1.jpg', 10, 10, 17);
+    //title
+    $pdf->SetFont('Helvetica', 'B', 14);
+    $pdf->Cell(190, 5, "PEMERINTAH PROVINSI JAWA TENGAH", 0, 1, 'C');
+    $pdf->SetFont('Helvetica', 'B', 14);
+    $pdf->Cell(190, 5, "BADAN PENGEMBANGAN ", 0, 1, 'C');
+    $pdf->Cell(190, 5, "SUMBER DAYA MANUSIA DAERAH", 0, 1, 'C');
+    $pdf->SetFont('Helvetica', '', 10);
+    $pdf->Cell(190, 3, "Jalan Setiabudi Nomor 201 A Semarang Kode Pos 50263", 0, 1, 'C');
+    $pdf->Cell(190, 3, "Telp. 024-7473066 Faks. 024-7473701", 0, 1, 'C');
+    $pdf->Cell(190, 3, "Website : www.bpsdmd.jatengprov.go.id Email : bpsdmd@jatengprov.go.id", 0, 1, 'C');
 
-// garis bawah double 
-$pdf->SetLineWidth(1);
-$pdf->Line(9,43,200,43);
-$pdf->SetLineWidth(0);
-$pdf->Line(9,43,200,43);
-$pdf->Ln(4);
+    // garis bawah double 
+    $pdf->SetLineWidth(1);
+    $pdf->Line(9, 43, 200, 43);
+    $pdf->SetLineWidth(0);
+    $pdf->Line(9, 43, 200, 43);
+    $pdf->Ln(4);
 
-// ISI
-$pdf->SetFont('Helvetica','',13);
-$pdf->Cell(190,5,"SURAT TUGAS",0,1,'C');
-$pdf->Cell(190,5,"NOMOR : 000.1.2.3.664",0,1,'C');
-$pdf->Ln(10); // Spasi
+    // ISI
+    $pdf->SetFont('Helvetica', '', 13);
+    $pdf->Cell(190, 5, "SURAT TUGAS", 0, 1, 'C');
+    $pdf->Cell(190, 5, "NOMOR : 000.1.2.3.664", 0, 1, 'C');
+    $pdf->Ln(10); // Spasi
 
 
 
     // Isi Surat
-    $pdf->MultiCell(0, 10, $no++ . '. ', 0, 'L');
-    $pdf->MultiCell(0, 10, $row['kode_produk'], 0, 'L', 0, 1); // Justify untuk rata kiri-kanan ('J')
-    
+    // $pdf->MultiCell(0, 10, $no++ . '. ', 0, 'L');
+    $no = 1;
+    while ($row = $result->fetch_assoc()) {
+        $pdf->MultiCell(0, 10, $no . '. ' .$row['kode_produk'], 0, 'L', 0, 1); // Justify untuk rata kiri-kanan ('J')
+        $no++;
+    }
+    // $pdf->MultiCell(0, 10, $row['kode_produk'], 0, 'L', 0, 1); // Justify untuk rata kiri-kanan ('J')
+
     $pdf->Ln(10); // Spasi
 
     // Output PDF ke browser
